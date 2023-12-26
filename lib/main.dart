306841db5808621213acb4af9b3abaadf8c767e1
Jhimi Rosales <desarrollo.uti5@heves.gob.pe>
2023-12-26 16:35:12 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+      title: "Mi App",
+      home: Inicio(),
+    );
+  }
+}
+
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mi Applicacion"),
+      ),
+      body: Center(child: Text("Hola Mundo")),
+    );
+  }
+}
