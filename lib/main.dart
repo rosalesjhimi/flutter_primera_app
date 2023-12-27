c97e711887589b5f4e6695062032176ef1691ef6
Jhimi Rosales <jhimi.2201@gmail.com>
2023-12-27 08:41:33 -0500

    [ADD] se agrega content de fotos

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,12 @@ class MiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
+    return MaterialApp(
       title: "Mi App",
       home: Inicio(),
     );
   }
 }
-
 
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
@@ -26,10 +25,41 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Mi Applicacion"),
-      ),
-      body: Center(child: Text("Hola Mundo")),
-    );
+        appBar: AppBar(
+          title: Text("Mi Applicacion"),
+        ),
+        body: _buildCuerpo());
   }
+}
+
+// Mostrando una listview de fotos
+Widget _buildCuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+            fit: BoxFit.cover)),
+    child: Center(child: Text("Hola")),
+  );
+}
+
+// Mostrando una listview de fotos
+Widget _buildBodyContentFotos() {
+  return ListView(
+    children: [
+      Container(
+          padding: EdgeInsets.all(20.0),
+          child: Image.network(
+              "https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
+      Container(
+          padding: EdgeInsets.all(20.0),
+          child: Image.network(
+              "https://images.unsplash.com/photo-1701205880113-b6309a1a7afe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
+      Container(
+          padding: EdgeInsets.all(20.0),
+          child: Image.network(
+              "https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
+    ],
+  );
 }
